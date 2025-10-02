1d44744fe263e673242939a3a2a61f3de6dcf669
pedro-henrique-oliveira <henriqueoliveiradasilvapedro1@gmail.com>
2025-10-02 11:30:34 -0300

    Add files via upload

--- Front/script.js
+++ Front/script.js
@@ -1,3 +1,35 @@
 const resultsDiv = document.getElementById("results")
 
-console.log(resultsDiv)
+async function fetchCarros(){
+    resultsDiv.innerHTML = "<p>Carregando...</p>"
+
+    try {
+        const response = await fetch('../Dados/data.json')
+        const data = await response.json()
+        //console.log(data)
+
+        if(data.error){
+            resultsDiv.innerHTML = "<p>Erro ao buscar produtos!!!</p>"
+            return
+        }
+        resultsDiv.innerHTML = ''
+
+        data.forEach(Carros => {
+            const card = document.createElement("div")
+            card.className = "card"
+            card.innerHTML = `
+                <img src="${Carros.imagem}" alt="${Carros.nome}">
+                <h2>${Carros.nome}</h2>
+                <h2>${Carros.descricao}</h3>
+                <p>$${Carros.preco.toFixed(3)}</p>              
+            `
+
+            resultsDiv.appendChild(card)
+        });
+
+    } catch (error) {
+         resultsDiv.innerHTML = "<p>Erro ao buscar produtos!!!</p>"
+    }
+}
+
+fetchCarros()
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Forja-manh-BD-01
 consumo de json
+
+tegs html exemplos:
+body
+div
+h1
+br
+p
+
+div class=""
+      |
+    estilo(css)
